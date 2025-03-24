2e99255b695021100ab15a2a5daa67a1333bd40a
tapasbrandstory <tapas@brandstory.in>
2025-03-24 10:56:08 +0530

    Mumbai

--- resources/views/frontend/signagesResion/leading-signage-company-in-mumbai.blade.php
+++ resources/views/frontend/signagesResion/leading-signage-company-in-mumbai.blade.php
@@ -89,6 +89,134 @@
     </div>
 </section>
 <x-coustom-inquiry-form />
+
+<section class="process-section">
+        <div class="container">
+            <h2 class="process-title">Hassle-Free Signage Solutions for Your Business</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="process-card">
+                        <div class="icon-design">
+                            <img src="{{asset('frontend/Images/graphic-designer.webp')}}" alt="On-demand Design">
+                        </div>
+                        <h3>On-demand Design</h3>
+                        <p>Our creative team of sign board makers in Mumbai offers flexible, personalized signage solutions. We transform your ideas into compelling visual narratives that strengthen your brand presence.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="process-card">
+                        <div class="icon-deliver">
+                            <img src="{{asset('frontend/Images/on-time.webp')}}" alt="Timely Deliver">
+                        </div>
+                        <h3>Timely Deliver</h3>
+                        <p>We optimize workflows, maintain stringent timelines, and coordinate logistics to guarantee
+                            project completion without compromising quality or design integrity.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="process-card">
+                        <div class="icon-purchase">
+                            <img src="{{asset('frontend/Images/transaction.webp')}}" alt="Direct Purchase">
+                        </div>
+                        <h3>Direct Purchase</h3>
+                        <p>Our streamlined purchasing pathway eliminates complexity, connecting you straight to creative
+                            brilliance.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<section>
+    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
+        <h2 class="hero-title">Types of Sign Boards We Manufacture</h2>
+        <p class="card-text text-center">As one of the best sign board manufacturers, we supply a variety of sign boards to fit in your organizational requirements. Here are the various kinds of sign boards we produce:</p>
+
+        <div class="row align-items-center">
+            <div class="col-lg-6 mb-4 mb-lg-0">
+                <div class="showcase-image">
+                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-13.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <h2 class="brand-title">Acrylic Sign Boards</h2>
+                <p class="brand-description">
+                    Our acrylic signage is ideal for indoor as well as outdoor usage. Lightweight and persistent, these sign boards can be tailored with your brand's logo and message. Acrylic led letters can also be attached for an illuminating outcome.
+                    <a href="{{route('premium_acrylic_sign_boards_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+                <h2 class="brand-title">Metal Sign Boards  </h2>
+                <p class="brand-description">
+                    For an executive and polished appearance, our stainless steel and ACP sign boards are perfect. These are ideal for building signage and generating a permanent impact for your business.
+                    <a href="{{route('stainless_steel_sign_board_manufacturer_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+                <h2 class="brand-title">Digital Signage  </h2>
+                <p class="brand-description">
+                    If you require vibrant and communicative signage, our digital signage solutions are the ideal preference. These compose of LED displays, LED signage boards, and other high-tech solutions to retain your audience engrossed.
+                    <a href="{{route('digital_signages_manufacturer_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
+
+        <div class="row align-items-center">
+
+            <div class="col-lg-6 order-2 order-lg-1">
+                <h2 class="brand-title">Outdoor Sign Boards</h2>
+                <p class="brand-description"> 
+                    Our outdoor signs are shaped to combat rough weather situations. Whether it’s an LED sign or a corrugated box array, we guarantee that your outdoor sign stays dynamic and observable for an elongated span.
+                    <a href="{{route('leading_signage_company_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+                <h2 class="brand-title">Interior Sign Boards  </h2>
+                <p class="brand-description">
+                    For your internal business, we produce fashionable and operational interior signs. This comprises wayfinding signage, office signage, and signage design personalized to your place and brand recognition.
+                    <a href="{{route('leading_signage_company_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+                <h2 class="brand-title">Neon Glowing Signage </h2>
+                <p class="brand-description">
+                     Neon glowing signs provide a retro and lively appearance that captures attention. We design custom neon-style LED sign boards and neon lighting for organizations that wish to be distinct and create a fearless statement.
+                    <a href="{{route('neon_sign_board_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
+                </p>
+            </div>
+
+            <div class="col-lg-6 mb-4 mb-lg-0  order-1 order-lg-2">
+                <div class="showcase-image">
+                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-14.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
+        <div class="row align-items-center">
+            <div class="col-lg-6 mb-4 mb-lg-0">
+                <div class="showcase-image">
+                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-15.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <h2 class="brand-title">LED Signs </h2>
+                <p class="brand-description">
+                    Our LED sign boards offer glowy, energy-saving solutions for your enterprise. Whether for exterior utility or as a factor of building signage, our LED signage alternatives are visually remarkable and simple to maintain.
+                </p>
+                <p class="brand-description">As a trustworthy sign board maker in Mumbai, we focus on supplying high-class signage that fulfills your exceptional needs. Select us for general sign work that uplifts your brand's presence!
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <section class="why-bg-light-pink py-5">
     <div class="container">
         <div class="text-center mb-5">
@@ -189,18 +317,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="why-card card">
-                                <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-55.webp')}}" class="why-card-img-top"
-                                    alt="Signage Manufacturing Company in Mumbai">
-                                <div class="card-body why-card-body px-0">
-                                    <h3 class="why-text-red mb-3"> Retail Businesses</h3>
-                                    <p class="card-text">We generate custom signage solutions such as acrylic LED letters, LED displays, and corrugated box signage that pull attention to your outlet, boosting your products and services efficiently.</p>
-                                </div>
-                            </div>
-                        </div>
+
                         <div class="col-md-4">
                             <div class="why-card card">
                                 <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-6.webp')}}" class="why-card-img-top"
@@ -323,92 +440,6 @@
     </div>
 </section>
 
-<section>
-    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
-        <h2 class="hero-title">Types of Sign Boards We Manufacture</h2>
-        <p class="card-text text-center">As one of the best sign board manufacturers, we supply a variety of sign boards to fit in your organizational requirements. Here are the various kinds of sign boards we produce:</p>
-
-        <div class="row align-items-center">
-            <div class="col-lg-6 mb-4 mb-lg-0">
-                <div class="showcase-image">
-                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-13.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-lg-6">
-                <h2 class="brand-title">Acrylic Sign Boards</h2>
-                <p class="brand-description">
-                    Our acrylic signage is ideal for indoor as well as outdoor usage. Lightweight and persistent, these sign boards can be tailored with your brand's logo and message. Acrylic led letters can also be attached for an illuminating outcome.
-                    <a href="{{route('premium_acrylic_sign_boards_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-                <h2 class="brand-title">Metal Sign Boards  </h2>
-                <p class="brand-description">
-                    For an executive and polished appearance, our stainless steel and ACP sign boards are perfect. These are ideal for building signage and generating a permanent impact for your business.
-                    <a href="{{route('stainless_steel_sign_board_manufacturer_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-                <h2 class="brand-title">Digital Signage  </h2>
-                <p class="brand-description">
-                    If you require vibrant and communicative signage, our digital signage solutions are the ideal preference. These compose of LED displays, LED signage boards, and other high-tech solutions to retain your audience engrossed.
-                    <a href="{{route('digital_signages_manufacturer_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
-
-        <div class="row align-items-center">
-
-            <div class="col-lg-6 order-2 order-lg-1">
-                <h2 class="brand-title">Outdoor Sign Boards</h2>
-                <p class="brand-description"> 
-                    Our outdoor signs are shaped to combat rough weather situations. Whether it’s an LED sign or a corrugated box array, we guarantee that your outdoor sign stays dynamic and observable for an elongated span.
-                    <a href="{{route('leading_signage_company_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-                <h2 class="brand-title">Interior Sign Boards  </h2>
-                <p class="brand-description">
-                    For your internal business, we produce fashionable and operational interior signs. This comprises wayfinding signage, office signage, and signage design personalized to your place and brand recognition.
-                    <a href="{{route('leading_signage_company_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-                <h2 class="brand-title">Neon Glowing Signage </h2>
-                <p class="brand-description">
-                     Neon glowing signs provide a retro and lively appearance that captures attention. We design custom neon-style LED sign boards and neon lighting for organizations that wish to be distinct and create a fearless statement.
-                    <a href="{{route('neon_sign_board_in_mumbai')}}" style="text-decoration:unset;color:#e43d12"> >> Know More</a>
-                </p>
-            </div>
-
-            <div class="col-lg-6 mb-4 mb-lg-0  order-1 order-lg-2">
-                <div class="showcase-image">
-                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-14.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container py-lg-5 py-md-3 py-sm-2 py-3">
-        <div class="row align-items-center">
-            <div class="col-lg-6 mb-4 mb-lg-0">
-                <div class="showcase-image">
-                    <img src="{{asset('frontend/Images/new/leading-signage-company-mumbai-15.webp')}}" alt="Signage Manufacturing Company in Mumbai" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-lg-6">
-                <h2 class="brand-title">LED Signs </h2>
-                <p class="brand-description">
-                    Our LED sign boards offer glowy, energy-saving solutions for your enterprise. Whether for exterior utility or as a factor of building signage, our LED signage alternatives are visually remarkable and simple to maintain.
-                </p>
-                <p class="brand-description">As a trustworthy sign board maker in Mumbai, we focus on supplying high-class signage that fulfills your exceptional needs. Select us for general sign work that uplifts your brand's presence!
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
 
 <section class="mt-lg-5">
     <div class="container py-lg-5 py-md-3 py-sm-2 py-3" id="gallery-container">
