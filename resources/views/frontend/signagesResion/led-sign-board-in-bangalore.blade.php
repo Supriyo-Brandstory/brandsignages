83adb61e820ead43242fceb7519773bff4b37257
tapasbrandstory <tapas@brandstory.in>
2025-04-23 11:28:53 +0530

    LED

--- resources/views/frontend/signagesResion/led-sign-board-in-bangalore.blade.php
+++ resources/views/frontend/signagesResion/led-sign-board-in-bangalore.blade.php
@@ -7,7 +7,7 @@
             <div class="carousel-item active">
                 <img src="{{asset('frontend/Images/led-signage1.webp')}}" alt="LED Sign Board manufacturers in Bangalore">
                 <div class="carousel-caption-custom">
-                    <h1>Custom LED Sign Boards in Bangalore</h1>
+                    <h1>Shine Smarter with Custom LED Signs in Bangalore</h1>
                      <div class="mt-4">
                         <a href="{{route('contact_us')}}" class="custom-btn">Get Started</a>
                         </div>
