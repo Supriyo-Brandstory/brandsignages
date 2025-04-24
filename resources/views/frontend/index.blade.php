0c407f0ccf30f76d3b1be202ce1c0a91025ace84
tapasbrandstory <tapas@brandstory.in>
2025-04-24 13:26:15 +0530

    1

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -16,9 +16,9 @@
                 <div class="carousel-item active">
                     <img src="{{asset('frontend/Images/banner1.webp')}}" alt="Signage Design for Engine Oil Brand by Our Signage Board Makers">
                     <div class="carousel-caption-custom">
-                        <h1>Shine Bright with the No. 1 Signage Makers in Bangalore</h1>
+                        <h1>Shine Bright with No. 1 Signage Makers in Bangalore</h1>
                         <div class="mt-4">
-                        <a  href="tel:+91 8006606080" class="custom-btn">Let's Talk</a>
+                        <a  href="tel:+91 8006606080" class="custom-btn">Get Quote</a>
                         </div>
                     </div>
                 </div>
@@ -26,7 +26,7 @@
                 <div class="carousel-item">
                     <img src="{{asset('frontend/Images/Banner.webp')}}" alt="Signage Design for Coffee Brand by Our Signage Manufacturer">
                     <div class="carousel-caption-custom">
-                        <h2>Get in Spotlight with stunning Signage Boards Bangalore</h2>
+                        <h2>Bold Signage Boards for Your Bold Brand in Bangalore</h2>
                         <div class="mt-4">
                         <a href="{{route('contact_us')}}" class="custom-btn">Get Quote</a>
                         </div>
@@ -36,7 +36,7 @@
                 <div class="carousel-item">
                     <img src="{{asset('frontend/Images/banner3.webp')}}" alt="Signage Design Sample by Brand Signages">
                     <div class="carousel-caption-custom">
-                        <h2>Make Your Mark with The Boldest Sign Boards in Bangalore</h2>
+                        <h2>Signage Boards Bangalore – Where Your Space Finds Its Unique Identity</h2>
                         <div class="mt-4">
                         <a href="{{route('contact_us')}}" class="custom-btn">Get Quote</a>
                         </div>
@@ -48,7 +48,7 @@
 
     <section>
         <div class="container py-5">
-            <h2 class="hero-title">Premium Signage Boards Bangalore:Crafted in Detail, Designed to Last</h2>
+            <h2 class="hero-title">Premium Signage Boards Bangalore: <br>Crafted in Detail, Designed to Last</h2>
 
             <div class="row align-items-center">
                 <div class="col-lg-6 mb-4 mb-lg-0">
@@ -265,68 +265,68 @@
     <section>
     <div class="container py-5" id="gallery-container">
         <h2 class="hero-title  mb-4">Our Recent Signage Projects</h2>
-        <p class=" text-center mb-5">We design high-quality and customized signage and sign boards for top businesses in Bangalore. From outdoor neon signs to sleek acrylic signages, digital signages, and LED sign boards, our solutions are designed to enhance both branding and decor.
+        <p class=" text-center mb-5">At Brand Signages, we design both indoor and outdoor signage to display your brand with style. We put our best efforts, skills, and precision into every signage board regardless of project size or budget.
         </p>            
         <div class="row align-items-center">
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-1.webp')}}" data-lightbox="Acrylic LED Signboard- Recent Client Work" data-title="Image 1">
-                    <img src="{{asset('frontend/Images/portfolio-1.webp')}}" class="img-fluid" alt="Acrylic LED Signboard- Recent Client Work" style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-1.webp')}}" data-lightbox="Acrylic LED Signboard" data-title="Image 1">
+                    <img src="{{asset('frontend/Images/portfolio-1.webp')}}" class="img-fluid" alt="Acrylic LED Signboard" style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-2.webp')}}" data-lightbox="LED Signboard Design- Recent Client Work" data-title="Image 2">
-                    <img src="{{asset('frontend/Images/portfolio-2.webp')}}" class="img-fluid" alt="LED Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-2.webp')}}" data-lightbox="LED Signboard Design" data-title="Image 2">
+                    <img src="{{asset('frontend/Images/portfolio-2.webp')}}" class="img-fluid" alt="LED Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-3.webp')}}" data-lightbox="LED Lightbox Design- Recent Client Work" data-title="Image 3">
-                    <img src="{{asset('frontend/Images/portfolio-3.webp')}}" class="img-fluid" alt="LED Lightbox Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-3.webp')}}" data-lightbox="LED Lightbox Design" data-title="Image 3">
+                    <img src="{{asset('frontend/Images/portfolio-3.webp')}}" class="img-fluid" alt="LED Lightbox Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-4.webp')}}" data-lightbox="Neon Signboard Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/portfolio-4.webp')}}" class="img-fluid" alt="Neon Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-4.webp')}}" data-lightbox="Neon Signboard Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/portfolio-4.webp')}}" class="img-fluid" alt="Neon Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-5.webp')}}" data-lightbox="Neon Signboard Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/portfolio-5.webp')}}" class="img-fluid" alt="Neon Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-5.webp')}}" data-lightbox="Neon Signboard Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/portfolio-5.webp')}}" class="img-fluid" alt="Neon Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-6.webp')}}" data-lightbox="Digital Signage Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/portfolio-6.webp')}}" class="img-fluid" alt="Digital Signage Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-6.webp')}}" data-lightbox="Digital Signage Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/portfolio-6.webp')}}" class="img-fluid" alt="Digital Signage Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-7.webp')}}" data-lightbox="Digital Signage Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/portfolio-7.webp')}}" class="img-fluid" alt="Digital Signage Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-7.webp')}}" data-lightbox="Digital Signage Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/portfolio-7.webp')}}" class="img-fluid" alt="Digital Signage Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/portfolio-8.webp')}}" data-lightbox="Acrylic LED Signboard Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/portfolio-8.webp')}}" class="img-fluid" alt="Acrylic LED Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/portfolio-8.webp')}}" data-lightbox="Acrylic LED Signboard Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/portfolio-8.webp')}}" class="img-fluid" alt="Acrylic LED Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/acrylic-led-sign-1.webp')}}" data-lightbox="Acrylic Signage Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/acrylic-led-sign-1.webp')}}" class="img-fluid" alt="Acrylic Signage Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/acrylic-led-sign-1.webp')}}" data-lightbox="Acrylic Signage Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/acrylic-led-sign-1.webp')}}" class="img-fluid" alt="Acrylic Signage Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/led-lightbox-5.webp')}}" data-lightbox="LED Signboard Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/led-lightbox-5.webp')}}" class="img-fluid" alt="LED Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/led-lightbox-5.webp')}}" data-lightbox="LED Signboard Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/led-lightbox-5.webp')}}" class="img-fluid" alt="LED Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/neon-5.webp')}}" data-lightbox="Neon Signboard Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/neon-5.webp')}}" class="img-fluid" alt="Neon Signboard Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/neon-5.webp')}}" data-lightbox="Neon Signboard Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/neon-5.webp')}}" class="img-fluid" alt="Neon Signboard Design"  style="width: 100%;">
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
-                    <a href="{{asset('frontend/Images/acrylic-led-sign-board-7.webp')}}" data-lightbox="Acrylic Signage Design- Recent Client Work" data-title="Image 4">
-                    <img src="{{asset('frontend/Images/acrylic-led-sign-board-7.webp')}}" class="img-fluid" alt="Acrylic Signage Design- Recent Client Work"  style="width: 100%;">
+                    <a href="{{asset('frontend/Images/acrylic-led-sign-board-7.webp')}}" data-lightbox="Acrylic Signage Design" data-title="Image 4">
+                    <img src="{{asset('frontend/Images/acrylic-led-sign-board-7.webp')}}" class="img-fluid" alt="Acrylic Signage Design"  style="width: 100%;">
                     </a>
                 </div>
             </div>
