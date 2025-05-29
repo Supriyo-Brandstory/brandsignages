6d22548ce877a14dce9557fa28711c7d8c4a43ab
tapasbrandstory <tapas@brandstory.in>
2025-05-29 17:39:06 +0530

    2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -73,7 +73,7 @@
                        metal signage for almost every industry. Contact the best signage manufacturers in Bangalore near you today!
                     </p>
                     <div class="mt-4">
-                        <a href="{{route('about-us')}}" class="custom-btn">Know About Us</a>
+                        <a href="{{route('about_us')}}" class="custom-btn">Know About Us</a>
                         </div>
                 </div>
             </div>
