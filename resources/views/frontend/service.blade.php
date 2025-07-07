ea08f8dbdaa8ed0111aa46318b678941b7467530
Supriyo-Brandstor <supriyo@brandstory.in>
2025-07-07 18:29:15 +0530

    ..

--- resources/views/frontend/service.blade.php
+++ resources/views/frontend/service.blade.php
@@ -24,70 +24,88 @@
                     <p class="new_servicespage-caption">Retail Signages & Retail Branding</p>
                 </div>
             </div>
-            <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+            <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
+
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-10.webp')}}" alt="Metal & Steel Signages"
                         class="img-fluid">
                     <p class="new_servicespage-caption">Metal & Steel Signages</p>
                 </div>
-            </div>
             </a>
 
+            </div>
+
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-9.webp')}}" alt="LED Signages" class="img-fluid">
                     <p class="new_servicespage-caption">LED Signages</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-8.webp')}}" alt="Outdoor Signages" class="img-fluid">
                     <p class="new_servicespage-caption">Outdoor Signages</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-7.webp')}}" alt="Interior Decals" class="img-fluid">
                     <p class="new_servicespage-caption">Interior Decals</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-6.webp')}}" alt="Digital Signages" class="img-fluid">
                     <p class="new_servicespage-caption">Digital Signages</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-5.webp')}}" alt="Reflective Signs" class="img-fluid">
                     <p class="new_servicespage-caption">Reflective Signs</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-4.webp')}}" alt="Event and Tradeshow Sign"
                         class="img-fluid">
                     <p class="new_servicespage-caption">Event and Tradeshow Sign</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-3.webp')}}" alt="Retractable Banners" class="img-fluid">
                     <p class="new_servicespage-caption">Retractable Banners</p>
                 </div>
+                  </a>
             </div>
 
             <div class="col-6 col-md-4 col-lg-4 new_servicespage-card">
+                  <a href="https://brandsignages.com/arcylic-signages" style="text-decoration: unset;">
                 <div class="new_servicespage-box">
                     <img src="{{asset('frontend/Images/new/service-2.webp')}}" alt="Indoor Signages" class="img-fluid">
                     <p class="new_servicespage-caption">Indoor Signages</p>
+                </a>
                 </div>
             </div>
         </div>
