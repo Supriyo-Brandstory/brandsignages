23b399d3cd33da5815e44c3a7c0b6552555ce23f
Supriyo-Brandstor <supriyo@brandstory.in>
2025-11-17 18:41:55 +0530

    ..

--- resources/views/frontend/layout/appLayout.blade.php
+++ resources/views/frontend/layout/appLayout.blade.php
@@ -412,6 +412,7 @@
                         <li><a href="/about-us" class="text-decoration-none">About Us</a></li>
                         <li><a href="/contact-us" class="text-decoration-none">Contact Us</a></li>
                         <li><a href="/case-studies" class="text-decoration-none">Recent Projects</a></li>
+                        <li><a href="href={{ asset('/frontend/Brand Signages Brochure-.pdf') }}" class="text-decoration-none">Download Brochure</a></li>
                         <li><a href="/terms-and-conditions" class="text-decoration-none">Terms & Conditions</a></li>
                         <li><a href="/privacy-policy" class="text-decoration-none">Privacy Policy</a></li>
                     </ul>
@@ -425,7 +426,7 @@
             </div>
         </div>
     </section>
-<a href="{{ asset('/frontend/Brand Signages Brochure-.pdf') }}" target="_blank" class="sticky-btn">Download Brochure</a>
+<a href="{{route('contact_us')}}" target="_blank" class="sticky-btn">Start Your Projects</a>
 
     <style>
         .footer a {
