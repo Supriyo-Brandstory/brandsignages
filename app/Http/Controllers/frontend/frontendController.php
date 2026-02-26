f54307a72133b52eab2ae2a6b32e86fff56eed9c
tapasbrandstory <tapas@brandstory.in>
2026-02-26 17:43:08 +0530

    ..

--- app/Http/Controllers/frontend/frontendController.php
+++ app/Http/Controllers/frontend/frontendController.php
@@ -338,11 +338,17 @@ class frontendController extends Controller
         $blogs = Blog::orderBy('id', 'desc')->where('blog_sub_category_id', 11)->take(3)->get();
         return view('frontend.signages.name-board-designs-for-shops-bangalore', compact('seo', 'blogs'));
     }
-    public function name_board_office_bangalore()
+    public function name_board_bangalore()
     {
         $currentRoute = Route::current()->uri();
         $seo = SEO::where('page_url', $currentRoute)->first();
-        return view('frontend.signages.name-board-design-for-office-bangalore', compact('seo'));
+        return view('frontend.signages.name-board-design-bangalore', compact('seo'));
+    }
+    public function name_board_design_bangalore()
+    {
+        $currentRoute = Route::current()->uri();
+        $seo = SEO::where('page_url', $currentRoute)->first();
+        return view('frontend.signages.name-board-design-bangalore', compact('seo'));
     }
     //chennai
     public function signage_company_in_chennai()
